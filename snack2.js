7842c9201539703b9b49e9080731e364bc84aa20
antonella1986 <antonella.leo.mail@gmail.com>
2025-06-05 16:08:43 +0200

    Clarify comment on memory reference behavior of cloned hamburger objects

--- snack2.js
+++ snack2.js
@@ -11,4 +11,4 @@ secondBurger.ingredients[0] = "Salad"; //abbiamo modificato il primo ingrediente
 console.log(hamburger.ingredients[0]); // 'Salad'
 console.log(secondBurger.ingredients[0]); // 'Salad', perché vengono copiate solo le proprietà di primo livello dell'oggetto con lo spread, e il valore di quella proprietà è un array, e l'array è un oggetto complesso. le proprietà annidate vengono copiate come RIFERIMENTO, e modificare una proprietà annidata nell'oggetto copiato modifica anche l'originale
 
-//QUINDI sono stati creati due oggetti in memoria, ma uno contiene ancora un riferimento condiviso
+//QUINDI sono stati creati due oggetti in memoria (escluso l'array), ma uno contiene ancora un riferimento condiviso
